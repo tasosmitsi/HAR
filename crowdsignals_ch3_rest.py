aa90a1fd03c52df2d7e2b5d561b10cd2b1a30abc
Tasos <tasos1109@gmail.com>
2021-06-16 21:54:00 +0200

    ch3 rest ton of modifications and improvements

--- crowdsignals_ch3_rest.py
+++ crowdsignals_ch3_rest.py
@@ -7,10 +7,10 @@
 #                                                            #
 ##############################################################
 
-import copy
 from pathlib import Path
 import pandas as pd
 import argparse
+import numpy as np
 
 from util.VisualizeDataset import VisualizeDataset
 from Chapter3.DataTransformation import LowPassFilter
@@ -19,12 +19,23 @@ from Chapter3.ImputationMissingValues import ImputationMissingValues
 from Chapter3.KalmanFilters import KalmanFilters
 
 # Set up the file names and locations.
-GRANULARITY = 6000
+GRANULARITY = 250
 SUBJECT_NAME = 'jeremy'
 DATA_PATH = Path('./intermediate_datafiles/')    
-DATASET_FNAME = 'HAR_3_' + SUBJECT_NAME + '_g' + GRANULARITY + '_result_outliers.csv'
-RESULT_FNAME = 'HAR_3_' + SUBJECT_NAME + '_g' + GRANULARITY + '_result_final.csv'
-ORIG_DATASET_FNAME = 'HAR_2_' + SUBJECT_NAME + '_g' + GRANULARITY + '.csv'
+DATASET_FNAME = 'HAR_3_' + SUBJECT_NAME + '_g' + str(GRANULARITY) + '_result_outliers.csv'
+RESULT_FNAME = 'HAR_3_' + SUBJECT_NAME + '_g' + str(GRANULARITY) + '_result_final.csv'
+ORIG_DATASET_FNAME = 'HAR_2_' + SUBJECT_NAME + '_g' + str(GRANULARITY) + '.csv'
+
+
+COLUMNS = ['roll_belt','pitch_belt','yaw_belt','total_accel_belt','gyros_belt_x',
+            'gyros_belt_y','gyros_belt_z','accel_belt_x','accel_belt_y','accel_belt_z',
+            'magnet_belt_x','magnet_belt_y','magnet_belt_z','total_accel_arm',
+            'gyros_arm_x','gyros_arm_y','gyros_arm_z','accel_arm_x','accel_arm_y','accel_arm_z','magnet_arm_x',
+            'magnet_arm_y','magnet_arm_z','roll_dumbbell','pitch_dumbbell','yaw_dumbbell','total_accel_dumbbell',
+            'gyros_dumbbell_x','gyros_dumbbell_y','gyros_dumbbell_z','accel_dumbbell_x','accel_dumbbell_y',
+            'accel_dumbbell_z','magnet_dumbbell_x','magnet_dumbbell_y','magnet_dumbbell_z','roll_forearm','pitch_forearm',
+            'yaw_forearm','total_accel_forearm','gyros_forearm_x','gyros_forearm_y','gyros_forearm_z','accel_forearm_x',
+            'accel_forearm_y','accel_forearm_z','magnet_forearm_x','magnet_forearm_y','magnet_forearm_z']
 
 
 def print_flags():
@@ -48,10 +59,9 @@ def main():
         raise e
 
     # We'll create an instance of our visualization class to plot the results.
-    DataViz = VisualizeDataset('HAR_3_final_{}_g{}'.format(SUBJECT_NAME, GRANULARITY))
+    DataViz = VisualizeDataset('HAR_3_final_{}_g{}_{}'.format(SUBJECT_NAME, GRANULARITY, FLAGS.mode))
     # Compute the number of milliseconds covered by an instance based on the first two rows
-    milliseconds_per_instance = (
-        dataset.index[1] - dataset.index[0]).microseconds/1000
+    milliseconds_per_instance = (dataset.index[1] - dataset.index[0]).to_numpy() / np.timedelta64(1, 's')
 
     MisVal = ImputationMissingValues()
     LowPass = LowPassFilter()
@@ -60,9 +70,9 @@ def main():
     if FLAGS.mode == 'imputation':
         # Let us impute the missing values and plot an example.
        
-        imputed_mean_dataset = MisVal.impute_mean(copy.deepcopy(dataset), 'hr_watch_rate')       
-        imputed_median_dataset = MisVal.impute_median(copy.deepcopy(dataset), 'hr_watch_rate')
-        imputed_interpolation_dataset = MisVal.impute_interpolate(copy.deepcopy(dataset), 'hr_watch_rate')
+        imputed_mean_dataset = MisVal.impute_mean(dataset.copy(), 'hr_watch_rate')       
+        imputed_median_dataset = MisVal.impute_median(dataset.copy(), 'hr_watch_rate')
+        imputed_interpolation_dataset = MisVal.impute_interpolate(dataset.copy(), 'hr_watch_rate')
         
         DataViz.plot_imputed_values(dataset, ['original', 'mean', 'median', 'interpolation'], 'hr_watch_rate',
                                     imputed_mean_dataset['hr_watch_rate'], 
@@ -96,11 +106,10 @@ def main():
         # Let us apply a lowpass filter and reduce the importance of the data above 1.5 Hz
 
         # Determine the sampling frequency.
-        fs = float(1000)/milliseconds_per_instance
+        fs = float(1)/milliseconds_per_instance
         cutoff = 1.5
         # Let us study acc_phone_x:
-        new_dataset = LowPass.low_pass_filter(copy.deepcopy(
-            dataset), 'acc_phone_x', fs, cutoff, order=10)
+        new_dataset = LowPass.low_pass_filter(dataset.copy(), 'acc_phone_x', fs, cutoff, order=10)
         DataViz.plot_dataset(new_dataset.iloc[int(0.4*len(new_dataset.index)):int(0.43*len(new_dataset.index)), :],
                              ['acc_phone_x', 'acc_phone_x_lowpass'], ['exact', 'exact'], ['line', 'line'])
 
@@ -125,8 +134,7 @@ def main():
 
         n_pcs = 7
 
-        dataset = PCA.apply_pca(copy.deepcopy(
-            dataset), selected_predictor_cols, n_pcs)
+        dataset = PCA.apply_pca(dataset.copy(), selected_predictor_cols, n_pcs)
 
         # And we visualize the result of the PC's
         DataViz.plot_dataset(dataset, ['pca_', 'label'], [
@@ -140,44 +148,34 @@ def main():
             dataset = MisVal.impute_interpolate(dataset, col)
 
         # And now let us include all LOWPASS measurements that have a form of periodicity (and filter them):
-        # periodic_measurements = ['acc_phone_x', 'acc_phone_y', 'acc_phone_z', 'acc_watch_x', 'acc_watch_y', 'acc_watch_z', 'gyr_phone_x', 'gyr_phone_y',
-        #                          'gyr_phone_z', 'gyr_watch_x', 'gyr_watch_y', 'gyr_watch_z', 'mag_phone_x', 'mag_phone_y', 'mag_phone_z', 'mag_watch_x',
-        #                          'mag_watch_y', 'mag_watch_z']
-        periodic_measurements = ['acc_phone_x', 'acc_phone_y', 'acc_phone_z', 'light_phone_illuminance', 'prox_phone_distance']
+        periodic_measurements = COLUMNS
 
         
         # Let us apply a lowpass filter and reduce the importance of the data above 1.5 Hz
 
         # Determine the sampling frequency.
-        fs = float(1000)/milliseconds_per_instance
+        fs = float(1)/milliseconds_per_instance
         cutoff = 1.5
 
         for col in periodic_measurements:
-            dataset = LowPass.low_pass_filter(
-                dataset, col, fs, cutoff, order=10)
+            dataset = LowPass.low_pass_filter(dataset, col, fs, cutoff, order=10)
             dataset[col] = dataset[col + '_lowpass']
             del dataset[col + '_lowpass']
 
         # We used the optimal found parameter n_pcs = 7, to apply PCA to the final dataset
-       
         selected_predictor_cols = [c for c in dataset.columns if (not ('label' in c)) and (not (c == 'hr_watch_rate'))]
         
-        n_pcs = 5
+        n_pcs = len(COLUMNS)
         
-        dataset = PCA.apply_pca(copy.deepcopy(dataset), selected_predictor_cols, n_pcs)
-        # And the overall final dataset:
-        # DataViz.plot_dataset(dataset, 
-        #                     ['acc_', 'gyr_', 'hr_watch_rate', 'light_phone_lux', 'mag_', 'press_phone_', 'pca_', 'label'],
-        #                     ['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
-        #                     ['line', 'line', 'line', 'line', 'line', 'line', 'line', 'points', 'points'])
+        dataset = PCA.apply_pca(dataset.copy(), selected_predictor_cols, n_pcs)
 
+        # And the overall final dataset:
         DataViz.plot_dataset(dataset, 
-                            ['acc_', 'light_phone_illuminance', 'prox_phone_distance', 'pca_', 'label'],
-                            ['like', 'like', 'like', 'like', 'like'],
-                            ['line', 'line', 'line', 'points', 'points'])
+                            COLUMNS + ['pca_', 'label'],
+                            ['exact'] * len(COLUMNS) + ['like', 'like'],
+                            ['line'] * len(COLUMNS) + ['points', 'points'])
 
         # Store the final outcome.
-
         dataset.to_csv(DATA_PATH / RESULT_FNAME)
 
 
@@ -193,5 +191,4 @@ if __name__ == '__main__':
 
    
     FLAGS, unparsed = parser.parse_known_args()
-
     main()
